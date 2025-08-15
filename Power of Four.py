5b4591214806a5a0eaa2111ecf84502dbb4087a6
Sydney Leigh <sydney.s.leigh@gmail.com>
2025-08-15 19:29:30 -0400

    I solved the Power Of Four problem. There are other solutions that are more efficient than mine, but I am proud of my solution because it made me think.

--- Power of Four.py
+++ Power of Four.py
@@ -29,13 +29,7 @@
  
 # Follow up: Could you solve it without loops/recursion?
 
-import math
-
-class Solution(object):
-    @staticmethod
-    def is_close(a, b, tol=1e-9):
-        return abs(a - b) < tol
-    
+class Solution(object):  
     def isPowerOfFour(self, n):
         """
         :type n: int
@@ -44,41 +38,40 @@ class Solution(object):
         
         #If n is 0 4^0 is 1 so this is not a power of four
         #If n is negative it can't be a power of four because 4^-n = 1/4^n
-        if n < 4:
-            return False        
+        # 4^0 = 1 so 1 is the only number below 4 that is a power of 4
+        # otherwise, 4 is the first power were 4^1 = 4
+        if n == 1:
+            return True
+        elif n < 4:
+            return False
         
         #4^x = (2^x * 2^x)
         #x = 1024
         #(2^y * 2^y) = 1024
         #y * y = 1024 = y^2 = sqrt(1024) = 32
         #1024 in binary is 0100 0000 0000
-        #Pattern realized that if we convert this to a binary and there is only one bit set
+        #Pattern realized that if we convert n to a binary and there is only one bit set
         # to 1 for the entire string of bits and the number of 0s is even then it must 
         # be a power of 4
         power = n
         binaryValue = str(bin(power)[2:])
-        #print(binaryValue)
-
-        #print("How many 1s do we have?")
         oneCount = binaryValue.count("1")
-
-        #print(oneCount)
 
         if oneCount == 1:
             #Count trailing 0's and see if they are odd or even, if even it's a power of 4
             #Find where 1 is:
             onePosition = binaryValue.find("1")
+            #Get the substring of all the 0s off after the 1
             zeros = binaryValue[onePosition+1:]
-            #print(zeros)
+            #Count number of zeros after the 1
             zerosLen = len(zeros)
-            #print("There are " + str(zerosLen) + " zeros.")
 
+            #If the length is divisible by 2 then it's an even number of 0s
             if (zerosLen % 2) == 0:
-                #print("Number of zeros is even! This is a power of four!")
+                #1 followed by even number of 0s is a binary number that is a power of 4
                 return True
-            #else:
-            #    print("Number of zeros is odd. This is not a power of four.")
     
+        #Number is not a power of four so we return False
         return False
 
 s = Solution()
@@ -91,6 +84,6 @@ def testCase():
 
 testCase()
 
-# checkNumber = 64
-# print("Is " + str(checkNumber) + " a power of 4?")
-# print(s.isPowerOfFour(checkNumber))
+checkNumber = 1
+print("Is " + str(checkNumber) + " a power of 4?")
+print(s.isPowerOfFour(checkNumber))
